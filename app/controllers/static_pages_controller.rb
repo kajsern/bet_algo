3fd19dde9cf7311ed519188d860d0131f2394205
Stefan Pärson <stefanparson@gmail.com>
2017-04-12 20:37:35 +0200

    basic pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def open
+  end
+
+  def history
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get open" do
+    get static_pages_open_url
+    assert_response :success
+  end
+
+  test "should get history" do
+    get static_pages_history_url
+    assert_response :success
+  end
+
+end
